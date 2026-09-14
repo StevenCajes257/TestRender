74d093b1c28c9e3973112fee9899c03e8c014db4
StevenCajes257 <michealjebarz717@gmail.com>
2026-09-14 10:31:26 +0800

    Allow production frontend CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://auth-messenger-frontend.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
